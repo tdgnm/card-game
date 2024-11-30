6f97edb51d0ea5df710884fdd6281b5fa807e5e8
tdgnm <meszarosgergo25@gmail.com>
2024-12-01 00:29:34 +0100

    Added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GameComponent } from './game/game.component';
+import { ItemsComponent } from './items/items.component';
+import { InfoComponent } from './info/info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'items', component: ItemsComponent },
+  { path: 'info', component: InfoComponent },
+];
