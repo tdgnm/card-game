8ebd4097729b40a8ee311487e39807ddaee00663
tdgnm <meszarosgergo25@gmail.com>
2024-12-05 14:35:50 +0100

    Created login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { NzCardComponent } from "ng-zorro-antd/card";
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NzCardComponent,
+    NzButtonModule,
+    NzCheckboxModule,
+    NzFormModule,
+    NzInputModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  validateForm;
 
+  constructor(private fb: NonNullableFormBuilder) {
+    this.validateForm = this.fb.group({
+      username: this.fb.control('', [Validators.required]),
+      password: this.fb.control('', [Validators.required]),
+      remember: this.fb.control(true)
+    });
+  }
+
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      // TODO: submit
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
 }
