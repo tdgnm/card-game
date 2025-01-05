4287022da34fc7bf94e48563501be4b0de6702b1
tdgnm <meszarosgergo25@gmail.com>
2025-01-05 22:04:50 +0100

    Integrated gameplay

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { BackComponent } from '../_components/back/back.component';
 import { HomeComponent } from '../_components/home/home.component';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { GAME_URL } from '../../globals';
 
 @Component({
   selector: 'app-game',
@@ -12,6 +14,30 @@ import { HomeComponent } from '../_components/home/home.component';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit, AfterViewInit, OnDestroy {
+  url: SafeResourceUrl;
+  @ViewChild('iframeElement', { static: true }) iframeElement!: ElementRef<HTMLElement>;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(GAME_URL);
+  }
+
+  ngAfterViewInit(): void {
+    const iframe = this.iframeElement.nativeElement;
+    iframe.onload = () => {
+      iframe.focus();
+    };
+  }
+
+  ngOnInit() {
+    window.addEventListener("message", this.receiveMessage, false);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener("message", this.receiveMessage, false);
+  }
+
+  receiveMessage(event: MessageEvent) {
+    console.log('Received message from iframe:', event.data);
+  }
 }
